dd5972545e3acfe3ebe8a6af490fc18ada3b14e8
hugoC123 <100361415+hugoC123@users.noreply.github.com>
2022-02-24 16:15:16 +0100

    Ajouter Course

--- src/dao/CourseDao.java
+++ src/dao/CourseDao.java
@@ -1,5 +1,14 @@
 package dao;
 
+import java.util.Date;
+import java.util.List;
+import model.Cheval;
+import model.Course;
+
 public class CourseDao {
+
+    public Course creerCourse(String nom, Date date, List<Cheval> chevaux) {
+        return new Course(nom, date, chevaux);
+    }
 
 }
