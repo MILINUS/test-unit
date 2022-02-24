618eb4e58830ac780b0a7f4aa2272c22b8bde611
hugoC123 <100361415+hugoC123@users.noreply.github.com>
2022-02-24 16:17:15 +0100

    Ajouter course

--- src/model/Course.java
+++ src/model/Course.java
@@ -4,7 +4,38 @@ import java.util.Date;
 import java.util.List;
 
 public class Course {
+
     public String nom;
     public Date date;
     public List<Cheval> chevaux;
+
+    public Course(String nom, Date date, List<Cheval> chevaux) {
+        this.nom = nom;
+        this.date = date;
+        this.chevaux = chevaux;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public List<Cheval> getChevaux() {
+        return chevaux;
+    }
+
+    public void setChevaux(List<Cheval> chevaux) {
+        this.chevaux = chevaux;
+    }
 }
