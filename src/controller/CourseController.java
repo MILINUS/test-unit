792010408f4d8c0f9b881155c7175e30b2fb8ba3
hugoC123 <100361415+hugoC123@users.noreply.github.com>
2022-02-24 16:14:48 +0100

    Ajouter course

--- src/controller/CourseController.java
+++ src/controller/CourseController.java
@@ -1,5 +1,19 @@
 package controller;
 
+import dao.CourseDao;
+import java.util.Date;
+import java.util.List;
+import model.Cheval;
+import model.Course;
+
 public class CourseController {
 
+    CourseDao courseDao;
+
+    public Course creerCourse(String nom, Date date, List<Cheval> chevaux) {
+        if (nom == null || date == null || chevaux == null || chevaux.size() != 6) {
+            return null;
+        }
+        return courseDao.creerCourse(nom, date, chevaux);
+    }
 }
