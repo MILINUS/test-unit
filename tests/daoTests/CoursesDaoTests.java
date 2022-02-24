c47967f1fa3a612e66226cfd7f6919705fee4d1a
Mari-Annaig Dupaya <32541696+yukii@users.noreply.github.com>
2022-02-24 15:29:11 +0100

    Init

---
+++ tests/daoTests/CoursesDaoTests.java
@@ -0,0 +1,14 @@
+package daoTests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CoursesDaoTests {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ tests/daoTests/HorsesDaoTests.java
@@ -0,0 +1,14 @@
+package daoTests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class HorsesDaoTests {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
