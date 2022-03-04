8a248bb79a208ff0a66d5492b625bfa12e6f0ab5
hugoC123 <100361415+hugoC123@users.noreply.github.com>
2022-03-04 09:46:00 +0100

    Add files via upload

--- src/controller/ChevalController.java
+++ src/controller/ChevalController.java
@@ -1,5 +1,60 @@
 package controller;
 
+import dao.ChevalDao;
+import model.Cheval;
+import model.Data;
+
 public class ChevalController {
+
+    ChevalDao chevalDao;
+
+    public ChevalController() {
+        chevalDao = new ChevalDao();
+    }
+
+    public boolean creerCheval(String nom, int age) {
+        // Gestion des erreurs
+        if (nom == null || age <= 0 || age > 50) {
+            if (age <= 0) {
+                System.out.println("Merci d'ajouter un cheval qui peut exister");
+            }
+            if (age > 50) {
+                System.out.println("Votre cheval très certainement mort");
+            }
+            return false;
+        }
+        for (Cheval cheval : Data.getChevaux()) {
+            if (cheval.getNom().equals(nom)) {
+                System.out.println("Un cheval avec le même nom existe déjà");
+                return false;
+            }
+        }
+        // -----------------------------------------
+        Cheval cheval = new Cheval(nom, age, 0);
+        return chevalDao.creerCheval(cheval);
+    }
+
+    public void voirChevaux() {
+        // Gestion des erreurs
+        if (Data.getChevaux().size() == 0) {
+            System.out.println("Aucun cheval dans la liste");
+        }
+        // -----------------------------------------
+        for (Cheval cheval : Data.getChevaux()) {
+            System.out.println("Nom : " + cheval.getNom() + " | Age : " + cheval.getAge() + " | Nombre de victoire : " + cheval.getNbVictoire());
+        }
+    }
+
+    public boolean supprimerChevalParNom(String nomCheval) {
+        Cheval cheval = chevalDao.getChevalParNom(nomCheval);
+        // Gestion des erreurs
+        if (cheval == null) {
+            System.out.println("Le cheval à supprimer n'existe pas");
+            return false;
+        }
+        // -----------------------------------------
+        chevalDao.supprimerCheval(cheval);
+        return true;
+    }
 
 }
--- src/controller/CourseController.java
+++ src/controller/CourseController.java
@@ -1,19 +1,83 @@
 package controller;
 
 import dao.CourseDao;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Random;
 import model.Cheval;
 import model.Course;
+import model.Data;
+import utilitaire.Utilitaire;
 
 public class CourseController {
 
     CourseDao courseDao;
-
-    public Course creerCourse(String nom, Date date, List<Cheval> chevaux) {
-        if (nom == null || date == null || chevaux == null || chevaux.size() != 6) {
-            return null;
-        }
-        return courseDao.creerCourse(nom, date, chevaux);
+    
+    public CourseController() {
+        courseDao = new CourseDao();
     }
+    
+    public boolean creerCourse(String nom, Date date) {
+        // Gestion des erreurs
+        if (nom == null || date == null) {
+            System.out.println("Erreur dans la création de la course");
+            return false;
+        }
+        if (date.before(Utilitaire.obtenirDateDeLaVeille())) {
+            System.out.println("La course à déjà eu lieu");
+            System.out.println("Veuillez saisir une autre date");
+            return false;
+        }
+        if (Data.getChevaux().size() < 6) {
+            System.out.println("Il manque des chevaux pour créer une course");
+            System.out.println("Veuillez créer au moins 6 chevaux auparavant");
+            return false;
+        }
+        // -----------------------------------------
+        System.out.println("Pas d'inquiétude les chevaux sont venus s'inscrire seul à la course");
+        
+        List<Cheval> chevaux = new ArrayList<>();
+        List<Cheval> listChevauxTemp = new ArrayList<>();
+        listChevauxTemp.addAll(Data.getChevaux());
+      
+        while (chevaux.size() != 6) {
+            Cheval chevalRandom = listChevauxTemp.get(new Random().nextInt(listChevauxTemp.size()));
+
+            chevaux.add(chevalRandom);
+            listChevauxTemp.remove(chevalRandom);
+        }  
+        
+        Course course = new Course(nom, date, chevaux);
+        return courseDao.creerCourse(course);
+    }
+    
+    public void voirCourses() {
+        // Gestion des erreurs
+        if (Data.getCourses().size() == 0) {
+            System.out.println("Aucune course dans la liste");
+        }
+        // -----------------------------------------
+        for (Course course : Data.getCourses()) {
+            System.out.println("Nom course : " + course.getNom() + " | Date : " + course.getDate());
+            System.out.println("Chevaux : ");
+            for (Cheval cheval : course.getChevaux()) {
+                System.out.println("Nom : " + cheval.getNom() + " | Age : " + cheval.getAge() + " | Victoire : " + cheval.getNbVictoire());
+            }  
+            System.out.println("");
+        }
+    }
+    
+    public boolean supprimerCourseParNom(String nomCourse) {
+        Course course = courseDao.getCourseParNom(nomCourse);
+         // Gestion des erreurs
+        if (course == null) {
+            System.out.println("La course à supprimer n'existe pas");
+            return false;
+        }
+        // -----------------------------------------
+        courseDao.supprimerCourse(course);
+        return true;
+    }
+    
 }
