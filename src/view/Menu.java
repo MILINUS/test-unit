cb77266606502dcc0d12bdd7b77aa30cee907a34
hugoC123 <100361415+hugoC123@users.noreply.github.com>
2022-03-07 23:56:26 +0100

    Add files via upload

--- src/view/Menu.java
+++ src/view/Menu.java
@@ -1,13 +1,25 @@
 package view;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import model.Cheval;
+import model.Data;
 import model.Hippodrome;
+import utilitaire.Utilitaire;
 
 public class Menu {
+    
+    
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Hippodrome hippodrome = new Hippodrome();
 
-        MenuView menuView = new MenuView();
+        // --- Initialisation de départ
+        List<Cheval> initChevaux = Utilitaire.lireFichierChevaux();
+        // ---
+        
+        MenuView menuView = new MenuView(initChevaux);
         while (true) {
             menuView.choixMenu();
         }
--- src/view/MenuView.java
+++ src/view/MenuView.java
@@ -6,7 +6,12 @@ package view;
 
 import controller.ChevalController;
 import controller.CourseController;
+import controller.HippodromeController;
+import java.io.IOException;
 import java.util.Date;
+import java.util.List;
+import model.Cheval;
+import model.Course;
 import utilitaire.Utilitaire;
 
 /**
@@ -15,12 +20,25 @@ import utilitaire.Utilitaire;
  */
 public class MenuView {
 
+    HippodromeController hippodromeController;
     CourseController courseController;
     ChevalController chevalController;
-
+    
     public MenuView() {
+        hippodromeController = new HippodromeController();
         courseController = new CourseController();
         chevalController = new ChevalController();
+    }
+
+    public MenuView(List<Cheval> chevaux) {
+        hippodromeController = new HippodromeController();
+        courseController = new CourseController();
+        chevalController = new ChevalController();
+        
+        
+         for (Cheval cheval : chevaux) {
+            chevalController.creerCheval(cheval.getNom(), cheval.getAge());
+        }
     }
 
     String courseNom;
@@ -39,6 +57,8 @@ public class MenuView {
         System.out.println("6 : Voir la liste des chevaux");
         System.out.println("7 : Modifier un cheval");
         System.out.println("8 : Supprimer un cheval");
+        System.out.println("9 : Lancer course");
+        System.out.println("10 : Résultats des dix dernières courses");
         switch (Utilitaire.saisieString()) {
             case "1":
                 System.out.println("-----------------------------------------------------------");
@@ -119,6 +139,27 @@ public class MenuView {
                 System.out.println("Saisissez le nom du cheval à supprimer : ");
                 chevalNom = Utilitaire.saisieString();
                 chevalController.supprimerChevalParNom(chevalNom);
+                System.out.println("-----------------------------------------------------------");
+                break;
+            case "9":
+                System.out.println("-----------------------------------------------------------");
+                 System.out.println("9 : Lancer course");
+                System.out.println("Saisissez le nom de la course à lancer : ");
+                courseNom = Utilitaire.saisieString();
+                Course course = courseController.obtenirCourseParNom(courseNom);
+                if (course != null) {
+                    Cheval cheval = courseController.lancerCourse(course);
+                    if (cheval != null) {
+                        chevalController.ajouterVictoireCheval(cheval);
+                        hippodromeController.saveResultat(course, cheval);
+                    }
+                }
+                System.out.println("-----------------------------------------------------------");
+                break;
+            case "10":
+                System.out.println("-----------------------------------------------------------");
+                System.out.println("10 : Résultats des dix dernières courses");
+                hippodromeController.lireDixResultats();
                 System.out.println("-----------------------------------------------------------");
                 break;
             default:
